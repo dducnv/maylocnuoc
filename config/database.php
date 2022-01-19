e4c4343d19e8047a04fcab2b4799626ddc63dad5
ducbe <ducbe2k2@gmail.com>
2022-01-19 10:54:10 +0700

    connect PG SQL

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('PGDB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('PGDB_URL'),
+            'host' => env('PGDB_HOST', '127.0.0.1'),
+            'port' => env('PGDB_PORT', '5432'),
+            'database' => env('PGDB_DATABASE', 'forge'),
+            'username' => env('PGDB_USERNAME', 'forge'),
+            'password' => env('PGDB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
