bcb1e924e0ccb85aef5b08cdf9aecfa3cb2b2c2b
ducbe <ducbe2k2@gmail.com>
2022-01-19 12:56:51 +0700

    connect PG SQL

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(1)->create();
+         \App\Models\User::factory(1)->create(
+             [
+                 'name' => "Admin",
+                 'email' => "admin@gmail.com",
+                 'role' => "ADMIN",
+                 'email_verified_at' => now(),
+                 'password' => 'admin@123', // password
+                 'remember_token' => Str::random(10),
+             ]
+         );
     }
 }
