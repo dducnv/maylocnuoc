ddeeeb46aea17c5214e4f607e3f1e0c8819cf393
ducbe <ducbe2k2@gmail.com>
2022-01-19 23:39:28 +0700

    .env commit

---
+++ resources/views/components/seo_meta.blade.php
@@ -0,0 +1,45 @@
+<title>HABECOM</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta charSet="UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta name="description" content="Hồ Sơ Của Nguyễn Văn Đức"/>
+<meta name="keywords"
+      content="Nguyễn Văn Đức, Nguyen Van Duc, Dduc NV, 7th.dec, FPT Aptech, FPT, Developer, Lập trình viên, Hà Nội, Việt Nam"/>
+<meta name="language" content="vi"/>
+<meta name="language" content="en"/>
+<meta http-equiv="content-language" content="vi"/>
+<meta http-equiv="content-language" content="en"/>
+{{--<link rel="shortcut icon" href={'/favicon.ico'}/>--}}
+{{--<link rel="apple-touch-icon" sizes="180x180" href={"apple-touch-icon.png"}/>--}}
+{{--<link rel="icon" type="image/png" sizes="32x32" href={"favicon-32x32.png"}/>--}}
+{{--<link rel="icon" type="image/png" sizes="16x16" href={"favicon-16x16.png"}/>--}}
+{{--<link rel="manifest" href={"site.webmanifest"}/>--}}
+{{--<link rel="mask-icon" href={"safari-pinned-tab.svg"} color="#5bbad5"/>--}}
+<meta name="msapplication-TileColor" content="#da532c"/>
+<meta name="theme-color" content="#ffffff"/>
+<meta name="copyright" content="Dduc NV"/>
+<meta name="author" content="Dduc NV, dduc7th.dec@gmail.com"/>
+<meta name="robots" content="noodp,index,follow"/>
+<meta name='revisit-after' content='1 days'/>
+<meta name="geo.region" content="VN-HN"/>
+<meta name="geo.placename" content="Hà Nội"/>
+<meta name="geo.position" content="21.001563, 105.797235"/>
+<meta name="ICBM" content="21.001563, 105.797235"/>
+<meta property="og:title"
+      content=""/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content={url}/>
+<meta property="og:image"
+      content="https://res.cloudinary.com/blogcuaduc/image/upload/v1641748957/cua-toi/z1jeda09imwmasq5tgwj.jpg"/>
+<meta name="url" content={url}/>
+<meta name="fb:page_id" content="NVD_7th.dec"/>
+<meta property="fb:app_id" content="NVD_7th.dec"/>
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:title" content={title}/>
+<meta name="twitter:description" content="Hồ Sơ Của Nguyễn Văn Đức"/>
+<meta name="twitter:image"
+      content="https://res.cloudinary.com/blogcuaduc/image/upload/v1641748957/cua-toi/z1jeda09imwmasq5tgwj.jpg"/>
+<meta property="twitter:url" content={url}/>
+<meta name="twitter:site" content="@dducnv0712"/>
+<meta name="og:email" content="dduc7th.dec@gmail.com"/>
+<meta name="og:phone_number" content=""/>
